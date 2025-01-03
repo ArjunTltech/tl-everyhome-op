e71ea23b30b8b3f48128ee741f71d2dfdd06c955
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2025-01-03 11:00:07 +0530

    first

---
+++ src/app/components/onePage/aboutSlider.js
@@ -0,0 +1,48 @@
+'use client'
+import React from "react"
+import Image from "next/image"
+import dynamic from "next/dynamic"
+
+const Slider = dynamic(() => import("react-slick"), { ssr: false })
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+import { aboutImg } from "../../data/onePage"
+
+export default function AboutSlider() {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 400,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        draggable: true,
+        arrows: true,
+        cssEase: "linear",
+        prevArrow: <button type="button" className="slick-prev"><i className="mdi mdi-chevron-left"></i></button>,
+        nextArrow: <button type="button" className="slick-next btn-primary"><i className="mdi mdi-chevron-right"></i></button>
+    }
+
+    return (
+        <div className="app-feature-shape position-relative">
+            <Slider {...settings}>
+                {aboutImg.map((item, index) => (
+                    <div key={index}>
+                        <Image 
+                            src={item} 
+                            width={0} 
+                            height={0} 
+                            sizes="100vw" 
+                            style={{width:'100%', height:'auto'}} 
+                            className="img-fluid" 
+                            alt=""
+                            priority={index === 0}
+                        />
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    )
+}
